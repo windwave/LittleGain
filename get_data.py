21fabae4dd00beda38d7b76aecd3aa60918a7af6
JohnWayne <zhengwei2015@gmail.com>
2018-07-02 23:47:10 +0800

    some test 20180702

--- get_data.py
+++ get_data.py
@@ -6,8 +6,9 @@ Created on Sat Jun 23 22:52:24 2018
 """
 import tushare as ts
 from sqlalchemy import create_engine
-
-
+import time
+from datetime import datetime
+import pandas as pd
 
 #get basic
 basics =ts.get_stock_basics()
@@ -39,11 +40,35 @@ for i in basics.index:
     #    hist_data_d = ts.get_hist_data(code,retry_count=5)
 #    hist_data_d.to_sql(('hist_data'+str(code)),engine)
 #basics.to_sql('basics',engine)
-
+t_series=pd.date_range(start=datetime(2018,6,1),end=datetime(2018,7,2),freq='D')
+#time_for_ticks = 
 for i in basics.index:
     tmp = str(i).zfill(6)
     print ('start to get ticks data'+tmp)
-    hist_day_data = ts.get_hist_data(tmp,retry_count=5)
+    for t in t_series:
+        time.sleep(10)
+        try:
+            hist_tick_data = ts.get_tick_data(tmp,date=str(t)[0:10],retry_count=5)
+        except:
+            continue
+        print str(t)[0:10]
+        hist_tick_data.time = str(t)[0:10] + ' ' +hist_tick_data.time
+        hist_tick_data= hist_tick_data.sort_values('time')
+        try:
+            hist_tick_data.to_sql(('hist_data_ticks_'+tmp),engine,index=True,if_exists='append')
+        except:
+            print ('store failure')
+            pass
+        print ('get data for '+ str(t)[0:10] +str(t)[0:10]+'complated')
     print ('get ticks data for'+tmp+'complated')
-    hist_day_data.index = hist_day_data.index.to_datetime()
-    hist_day_data.to_sql(('hist_data_ticks_'+tmp),engine)
+    
+for i in basics.index:
+    tmp = str(i).zfill(6)
+    print ('start to drop table'+tmp)
+    sql=str('DROP TABLE IF EXISTS '+'hist_data_ticks_'+tmp)
+    engine.execute(sql)
+    print ('drop table hist_data_ticks_'+tmp+' complated')
+    
+    
+data_20180702 = ts.get_today_all()
+data_20180702.to_sql('data_20180702',engine)
